dfa2f7371e9cc4a7b645fdd32f8f2a7dd4913ac2
Alexey <lexshadrin@mail.ru>
2022-07-21 16:23:22 +1100

    Первый коммит

---
+++ src/main/java/pro/sky/course_2_lesson_12/Course2Lesson12Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course_2_lesson_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Lesson12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Lesson12Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/course_2_lesson_12/Course2Lesson12ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course_2_lesson_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2Lesson12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
